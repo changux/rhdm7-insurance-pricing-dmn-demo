7e82ad1b44327c8d0ec460be27c86d4bbe704e75
PG <changux@users.noreply.github.com>
2018-11-18 19:16:39 -0500

    Create fix-artifacts.sh

---
+++ installs/fix-artifacts.sh
@@ -0,0 +1,3 @@
+#!/bin/sh
+ln -s rhdm-7.1.0-kie-server-ee7.zip rhdm-7.0.0.GA-kie-server-ee7.zip
+ln -s rhdm-7.1.0-decision-central-eap7-deployable.zip rhdm-7.0.0.GA-decision-central-eap7-deployable.zip
